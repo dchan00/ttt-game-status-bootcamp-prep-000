7cae82788e2e0e4c7ebaeefa904f92bd908eb36f
DANIEL CHAN <dchan513@msn.com>
2017-10-04 02:20:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -60,9 +60,9 @@ end
 def draw?(board)
   if won?(board)
     return false
-  elsif(!won?(board) && full?(board))
+  elsif !won?(board) && full?(board)
     return true
-  elsif(!won?(board) && !full?(board))
+  elsif !won?(board) && !full?(board)
     return false
   end
 end
