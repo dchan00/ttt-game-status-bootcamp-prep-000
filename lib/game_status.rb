7cfa21b045b7e4657189fe6cafce0496d8767f21
DANIEL CHAN <dchan513@msn.com>
2017-10-04 02:09:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,7 +58,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if(won?(board).is_a? Array)
+  if(!won?(board).is_a? Array)
     return "asdfasdf"
   elsif(!won?(board) && full?(board))
     return true
